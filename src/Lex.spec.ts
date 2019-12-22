d90252d499a4b3f1672ecf360cf67c0e24778517
janeajian <janeajian@tencent.com>
2019-12-22 17:12:46 +0800

    feat(lex): simple lex parese

--- src/Lex.spec.ts
+++ src/Lex.spec.ts
@@ -1,13 +1,13 @@
-import  Lex  from './Lex';
+import Lex from './Lex';
 
-describe('TypeScript WebPack Starter Tests', () => {
-  it('A good way to start building an awesome library is by doing Unit Tests 👌🏽', () => {
-    const lex = new Lex();
-    //const result = lex.executeParse();
-    //expect(result >= 0 && result <= 10).toBeTruthy();
-    const result = lex.executeParse('age >= 45');
-    expect(result === '').toBeTruthy();
-  });
+describe('Lex Starter Tests', () => {
+    it('const age = 16', () => {
+        const lex = new Lex();
+        //const result = lex.executeParse();
+        //expect(result >= 0 && result <= 10).toBeTruthy();
+        const result = lex.executeParse('const age = 18');
+        expect(result === 'Const:const;Identifier:age;Assignment:=;ConstDigit:18').toBeTruthy()
+    });
 });
 
 
