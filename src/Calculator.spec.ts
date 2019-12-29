04a6e6a1c8605925d61ce548357283ad152ef022
janeluck <janeluck158@gmail.com>
2019-12-29 22:24:59 +0800

    ok

--- src/Calculator.spec.ts
+++ src/Calculator.spec.ts
@@ -1,17 +1,16 @@
 import Lex from './Lex';
 import {printTokens} from './Lex';
-import {primary} from './Calculator';
-
+import {primary, dumpAST} from './Calculator';
+import Calculator from './Calculator';
+const calculator = new Calculator();
 describe('Calculator Starter Tests', () => {
     it('astnode', () => {
-        const lex = new Lex();
         //const result = lex.executeParse();
         //expect(result >= 0 && result <= 10).toBeTruthy();
-        const result1 = printTokens(lex.executeParse('2 * 3 + 5'));
-        const result2 = lex.executeParse('2 + 3');
-        expect(result1 === 'ConstDigit:2;Star:*;ConstDigit:3;Plus:+;ConstDigit:5').toBeTruthy();
-        console.log(result2);
-        console.log(primary(result2));
+       // const result1 = printTokens(lex.executeParse('2 * 3 + 5'));
+        const result2 = calculator.executeParse('const lego = 99');
+        //expect(result1 === 'ConstDigit:2;Star:*;ConstDigit:3;Plus:+;ConstDigit:5').toBeTruthy();
+        console.log(dumpAST(result2));
     });
 });
 
--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,25 +1,29 @@
 import {Token, TokenType} from './Lex';
 import Lex from './Lex';
+
 const lex = new Lex();
+
 enum ASTNodeType {
-    Program,
-    ConstDeclaration,
-    AdditiveExp,
-    multiplicativeExp,
-    Identifier
+  Program,
+  ConstDeclaration,
+  AdditiveExp,
+  multiplicativeExp,
+  Identifier
 }
 
 class Calculator {
-    constructor() {
+  constructor() {
+  }
 
+  executeParse(scripts: string) {
+    const tokens = lex.executeParse(scripts);
+    const node = new SimpleASTNode(ASTNodeType.Program, 'Calculator');
+    const child = additive(tokens);
+    if (child) {
+      node.addChild(child);
     }
-
-    executeParse(scripts: string) {
-        const tokens =
-        const node = new SimpleASTNode(ASTNodeType.Program, 'Calculator');
-        const child = additive()
-
-    }
+    return node;
+  }
 }
 
 // const语句的文法 constDeclaration : Const Identifier ( '=' additiveExpression)?;
@@ -42,112 +46,115 @@ class Calculator {
 }*/
 
 class SimpleASTNode {
-    parent?: SimpleASTNode;
-    children: Array<SimpleASTNode>;
-    nodeType: ASTNodeType;
-    nodeText: string;
+  parent?: SimpleASTNode;
+  children: Array<SimpleASTNode>;
+  nodeType: ASTNodeType;
+  nodeText: string;
 
-    constructor(nodeType: any, nodeText: string) {
-        this.children = [];
-        this.nodeType = nodeType;
-        this.nodeText = nodeText;
-    }
+  constructor(nodeType: any, nodeText: string) {
+    this.children = [];
+    this.nodeType = nodeType;
+    this.nodeText = nodeText;
+  }
 
-    addChild(node: SimpleASTNode) {
-        this.children.push(node)
-    }
+  addChild(node: SimpleASTNode) {
+    this.children.push(node)
+  }
 }
 
 // 基础表达式
 function primary(tokens: Array<Token>) {
-    let node = null;
-    let token = tokens.shift();
-    if (token !== null) {
-        if (token.type === TokenType.Const) {
-            token = tokens.shift();
-            node = new SimpleASTNode(ASTNodeType.ConstDeclaration, token.text)
-        } else if (token.type  === TokenType.Identifier) {
-            token = tokens.shift();
-            node = new SimpleASTNode(ASTNodeType.Identifier, token.text);
-        } else {
-            throw new Error("expecting an additive expression")
-        }
+  let node = null;
+  let token = tokens.shift();
+  if (token !== null) {
+    if (token.type === TokenType.Const) {
+      token = tokens.shift();
+      node = new SimpleASTNode(ASTNodeType.ConstDeclaration, token.text)
+    } else if (token.type === TokenType.Identifier) {
+      token = tokens.shift();
+      node = new SimpleASTNode(ASTNodeType.Identifier, token.text);
+    } else {
+      throw new Error("expecting an additive expression")
     }
-    return node;
+  }
+  return node;
 }
 
 function multiplicative(tokens: Array<Token>) {
-    let child1 = primary(tokens);
-    let node = child1;
-    let token = tokens.shift();
-    if (child1 != null && token != null) {
-        if (token.type === TokenType.Star) {
-            token = tokens.shift();
-            let child2 = primary(tokens);
-            if (child2 != null) {
-                node = new SimpleASTNode(ASTNodeType.multiplicativeExp, token.text);
-                node.addChild(child1);
-                node.addChild(child2);
-            } else {
-                throw new Error("invalid multiplicative expression, expecting the right part.");
-            }
-        }
+  let child1 = primary(tokens);
+  let node = child1;
+  let token = tokens.shift();
+  if (child1 != null && token != null) {
+    if (token.type === TokenType.Star) {
+      token = tokens.shift();
+      let child2 = primary(tokens);
+      if (child2 != null) {
+        node = new SimpleASTNode(ASTNodeType.multiplicativeExp, token.text);
+        node.addChild(child1);
+        node.addChild(child2);
+      } else {
+        throw new Error("invalid multiplicative expression, expecting the right part.");
+      }
     }
-    return node;
+  }
+  return node;
 }
 
 function additive(tokens: Array<Token>) {
-    let child1 = multiplicative(tokens);
-    let node = child1;
-    let token = tokens.shift();
-    if (child1 != null && token != null) {
-        if (token.type === TokenType.Plus) {
-            token = tokens.shift();
-            let child2 = multiplicative(tokens);
-            if (child2 != null) {
-                node = new SimpleASTNode(ASTNodeType.AdditiveExp, token.text);
-                node.addChild(child1);
-                node.addChild(child2);
-            } else {
-                throw new Error("invalid multiplicative expression, expecting the right part.");
-            }
-        }
+  let child1 = multiplicative(tokens);
+  let node = child1;
+  let token = tokens.shift();
+  if (child1 != null && token != null) {
+    if (token.type === TokenType.Plus) {
+      token = tokens.shift();
+      let child2 = multiplicative(tokens);
+      if (child2 != null) {
+        node = new SimpleASTNode(ASTNodeType.AdditiveExp, token.text);
+        node.addChild(child1);
+        node.addChild(child2);
+      } else {
+        throw new Error("invalid multiplicative expression, expecting the right part.");
+      }
     }
-    return node;
+  }
+  return node;
 }
 
 function constDeclare(tokens: Array<Token>) {
-    let node = null;
-    let token = tokens.shift();
-    if (token !== null && token.type === TokenType.Const) {   //匹配Const
-        token = tokens.shift();
-        if (token !== null && token.type === TokenType.Identifier) { //匹配标识符
-            //创建当前节点，并把变量名记到AST节点的文本值中
-            node = new SimpleASTNode(ASTNodeType.ConstDeclaration, token.text);
-            token = tokens.shift();
-            // 匹配等号
-            if (token != null && token.type === TokenType.Assignment) {
-                // 匹配表达式
-                const child = additive(tokens);
-                if (child == null) {
-                    throw new Error("SyntaxError: Missing initializer in const declaration");
-                } else {
-                    node.addChild(child);
-                }
-            }
+  let node = null;
+  let token = tokens.shift();
+  if (token !== null && token.type === TokenType.Const) {   //匹配Const
+    token = tokens.shift();
+    if (token !== null && token.type === TokenType.Identifier) { //匹配标识符
+      //创建当前节点，并把变量名记到AST节点的文本值中
+      node = new SimpleASTNode(ASTNodeType.ConstDeclaration, token.text);
+      token = tokens.shift();
+      // 匹配等号
+      if (token != null && token.type === TokenType.Assignment) {
+        // 匹配表达式
+        const child = additive(tokens);
+        if (child == null) {
+          throw new Error("SyntaxError: Missing initializer in const declaration");
         } else {
-            // 不允许空语句
-            throw new Error("SyntaxError: const name expected");
+          node.addChild(child);
         }
+      }
+    } else {
+      // 不允许空语句
+      throw new Error("SyntaxError: const name expected");
     }
-    return node;
+  }
+  return node;
 }
+
 function dumpAST(node: SimpleASTNode) {
-    console.log('\t' + node.nodeType + " " + node.nodeText);
-    node.children && node.children.forEach(dumpAST);
+  console.log('\t' + ASTNodeType[node.nodeType] + " " + node.nodeText);
+  node.children && node.children.forEach(dumpAST);
 }
+
 export default Calculator
 export {
-    constDeclare,
-    primary
+  constDeclare,
+  primary,
+  dumpAST
 }
